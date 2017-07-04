a9c0d7a80ac681fdacaa58151e88e97d70b4edf9
Rafael <rafacdb@gmail.com>
2017-07-04 20:53:14 -0300

    alter paginate

--- src/Paginate.js
+++ src/Paginate.js
@@ -5,14 +5,14 @@ import styled from 'styled-components';
 
 const PaginateStyled = styled.ul`
 display: flex;
-flex-wrap: wrap;
+flex-wrap: nowrap;
 justify-content: center;
 align-items: center;
 list-style-type: none;
 
 li a {
   display: flex;
-  flex-wrap: wrap;
+  flex-wrap: nowrap;
   justify-content: center;
   align-items: center;
 }
