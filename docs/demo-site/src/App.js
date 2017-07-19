5d8b3d3010f3607e7bb5ea70359d39edcf07496f
Rafael Bastiani <rafacdb@gmail.com>
2017-07-19 12:43:42 -0300

    working in demo site

--- docs/demo-site/src/App.js
+++ docs/demo-site/src/App.js
@@ -1,20 +1,109 @@
 import React, { Component } from "react";
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider, injectGlobal } from "styled-components";
 
-import { Form, FormFields, Input, Button } from "@rafacdb/bah";
+import {
+  Form,
+  FormFields,
+  Input,
+  Button,
+  LayoutAdmin,
+  Header,
+  Nav,
+  Article,
+  Footer,
+  Sidebar,
+  SidebarItem
+} from "@rafacdb/bah";
+
 import theme from "@rafacdb/bah/src/theme";
+
+function DemoForm() {
+  return (
+    <Form>
+      <FormFields>
+        <Input>Default</Input>
+        <Input isValid>isValid</Input>
+        <Input isInvalid>isInvalid</Input>
+        <Button primary inline>
+          primary
+        </Button>
+        <Button success inline>
+          success
+        </Button>
+        <Button info inline>
+          info
+        </Button>
+        <Button warning inline>
+          warning
+        </Button>
+        <Button danger inline>
+          danger
+        </Button>
+        <Button inverse inline>
+          inverse
+        </Button>
+        <Button primary inline small>
+          primary small
+        </Button>
+        <Button success inline large>
+          success large
+        </Button>
+        <Button info inline xlarge>
+          info xlarge
+        </Button>
+      </FormFields>
+    </Form>
+  );
+}
 
 class App extends Component {
   render() {
+    // eslint-disable-next-line
+    injectGlobal`
+      * {
+        margin: 0;
+        padding: 0;
+      }
+      html,
+      body {
+        width: 100%;
+        height: 100%;
+        font-family: ${theme.bahFontFamily};
+        color: ${theme.bahDefaultTextColor};
+        background-color: ${theme.bahDefaultBackgroundColor};
+      }
+    `;
     return (
       <div>
         <ThemeProvider theme={theme}>
-          <Form>
-            <FormFields>
-              <Input>Test1</Input>
-              <Button success>Salvar</Button>
-            </FormFields>
-          </Form>
+          <div>
+            <Header>header</Header>
+            <LayoutAdmin>
+              <Article>
+                <DemoForm />
+              </Article>
+              <Nav>
+                <Sidebar>
+                  <SidebarItem>
+                    <input type="checkbox" id="menu1" />
+                    <label htmlFor="menu1">Principal</label>
+                    <ul>
+                      <li>
+                        <a href="#">Home</a>
+                      </li>
+                      <li>
+                        <a href="#">Dashboard</a>
+                      </li>
+                      <li>
+                        <a href="#">About</a>
+                      </li>
+                    </ul>
+                  </SidebarItem>
+                </Sidebar>
+              </Nav>
+            </LayoutAdmin>
+            <Footer>Footer</Footer>
+          </div>
         </ThemeProvider>
       </div>
     );
